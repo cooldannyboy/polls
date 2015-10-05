a5a88778b9585ac21607c75deed865551cf6c521
DannyC <cooldannyboy@gmail.com>
2015-10-05 22:49:10 +0800

    first commit

---
+++ polls/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+#from polls_app.views import index
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'polls.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^polls_app/', include('polls_app.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+#    url(r'^test/$', index),
+)
